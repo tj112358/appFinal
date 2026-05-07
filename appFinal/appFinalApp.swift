e942959881b4ea8d3e0ac795aead09c4fa5894d2
Theadora Yocum <theayocum@mac.com>
2026-05-07 11:23:46 -0400

    Initial Commit

---
+++ appFinal/appFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  appFinalApp.swift
+//  appFinal
+//
+//  Created by Thea Yocum on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct appFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
